5e0259e79d9bd4a1274427124cf3a8aeeff0019e
Ryan Rymarczyk <rymarczy@gmail.com>
2024-01-19 09:01:22 -0500

    disable github jobs

--- src/research_etl/pipeline.py
+++ src/research_etl/pipeline.py
@@ -3,8 +3,8 @@ import os
 from research_etl.etl_gtfs.gtfs_job import run as gtfs_job
 from research_etl.etl_korbato.korbato_job import run as odx_job
 from research_etl.etl_afc.afc_job import run as afc_job
-from research_etl.mbta_open_data.csat_job import run as csat_job
-from research_etl.mbta_open_data.gse_job import run as gse_job
+# from research_etl.mbta_open_data.csat_job import run as csat_job
+# from research_etl.mbta_open_data.gse_job import run as gse_job
 
 from research_etl.utils.util_aws import check_for_parallel_tasks
 from research_etl.utils.util_rds import DatabaseManager
@@ -23,8 +23,8 @@ def run_jobs() -> None:
     gtfs_job(db_manager)
     odx_job(db_manager)
     afc_job(db_manager)
-    csat_job(db_manager)
-    gse_job(db_manager)
+    # csat_job(db_manager)
+    # gse_job(db_manager)
 
 
 if __name__ == "__main__":
