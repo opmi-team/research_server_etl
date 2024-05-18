0b205d2d388ea3f5820a67abc8d6fcd45a778600
Ryan Rymarczyk <rymarczy@gmail.com>
2024-05-18 16:12:16 -0400

    disable odx 20240517

--- src/research_etl/pipeline.py
+++ src/research_etl/pipeline.py
@@ -2,7 +2,8 @@ import os
 
 from research_etl.etl_gtfs.gtfs_job import run as gtfs_job
 from research_etl.etl_korbato.korbato_job import run as odx_job
-from research_etl.etl_korbato.korbato_job import alt_run as odx_catch_up_job
+
+# from research_etl.etl_korbato.korbato_job import alt_run as odx_catch_up_job
 
 from research_etl.etl_afc.afc_job import run as afc_job
 
@@ -25,7 +26,7 @@ def run_jobs() -> None:
 
     gtfs_job(db_manager)
     odx_job(db_manager)
-    odx_catch_up_job(db_manager)
+    # odx_catch_up_job(db_manager)
     afc_job(db_manager)
     # csat_job(db_manager)
     # gse_job(db_manager)
